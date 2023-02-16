bae1184bbceb7207fe3f373121b9a9c477a2dd59
Lucas Puziski <lpuziski@hotmail.com>
2023-02-16 16:18:53 -0300

    MySQL connection and first migration to DB

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -1,9 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMVC.Data;
+
+// Replace with your connection string.
+var connectionString = "server=localhost;userid=root;password=1234567;database=saleswebmvcappdb";
+
+// Replace with your server version and type.
+// Use 'MariaDbServerVersion' for MariaDB.
+// Alternatively, use 'ServerVersion.AutoDetect(connectionString)'.
+// For common usages, see pull request #1233.
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 32));
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<SalesWebMVCContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMVCContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMVCContext' not found.")));
+builder.Services.AddDbContext<SalesWebMVCContext>(options => options.UseMySql(connectionString, serverVersion)
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors());
+
+//builder.Configuration.GetConnectionString("SalesWebMVCContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMVCContext' not found.")));
 
 //app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
 // Add services to the container.
