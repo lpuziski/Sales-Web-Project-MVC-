8b62922f1f74422d3c1013f99b9794c57fb3cdef
Lucas Puziski <lpuziski@hotmail.com>
2023-02-17 12:30:03 -0300

    Seller Service and basic FindAll

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMVC.Data;
+using SalesWebMVC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,7 @@ builder.Services.AddDbContext<SalesWebMVCContext>(options => options.UseMySql(co
                 //.EnableDetailedErrors());
 
 builder.Services.AddScoped<SeedingService>();
+builder.Services.AddScoped<SellerService>();
 
 
 var app = builder.Build();
