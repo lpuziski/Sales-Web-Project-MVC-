4387826ee767821fadd05d22a1a37b30682808a5
Lucas Puziski <lpuziski@hotmail.com>
2023-02-17 10:08:11 -0300

    Other entities and second migration

---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+using System.Linq;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthdate { get; set; }
+        public double Salary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime birthdate, double salary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Birthdate = birthdate;
+            Salary = salary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord record)
+        {
+            Sales.Add(record);
+        }
+
+        public void RemoveSales(SalesRecord record)
+        {
+            Sales.Remove(record);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
