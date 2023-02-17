18dd25a1323b2a68ade023211bf21087032bc730
Lucas Puziski <lpuziski@hotmail.com>
2023-02-17 19:32:45 -0300

    Validation

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -7,19 +7,27 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}.")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required.")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required.")]
         [Display(Name="Date of Birth")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Birthdate { get; set; }
 
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
+        [Required(ErrorMessage = "{0} required.")]
         [DataType(DataType.Currency)]
         [DisplayFormat(DataFormatString ="{0:F2}")]
         public double Salary { get; set; }
+
         public Department Department { get; set; }
         [Display(Name="Department ID")]
         public int DepartmentId { get; set; }
